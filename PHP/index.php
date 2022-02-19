96b031d1d96a1f6ac4774abac508e9176d6ea85c
Juan <dasaiacos@gmail.com>
2022-02-18 19:46:49 -0500

    Curso_POO_18.02.22

--- PHP/index.php
+++ PHP/index.php
@@ -4,7 +4,7 @@ require_once("uberX.php");
 require_once("uberPool.php");
 require_once("account.php");
 
-$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX = new UberX("CVB123", new Account("Juan Hernandez", "AND456"), "Chevrolet", "Spark");
 $uberX->printDataCar();
 
 $uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
